9b8fd78bfc5ee58eb52b5b31156debe1499a9f71
Sujoy <sujoy60@hotmail.com>
2017-11-06 00:52:25 +1100

    Test project added and unit test methods defined with AAA Arracnge act and assert principles.

---
+++ TableTopRobo.Tests/RobotTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TableTopRobo;
+
+namespace TableTopRobo.Tests
+{
+    [TestClass]
+    public class RobotTest
+    {
+        [TestMethod]
+        public void Robot_InitialisedButNotPlaced_CannotBeMoved()
+        {
+            var robot = new Robot();
+            var result = robot.Move();
+            Assert.IsFalse(result);
+            Assert.AreEqual("Robot cannot move until it has been placed on the table.", robot.LastError);
+        }
+
+
+        [TestMethod]
+        public void Robot_PlacedAndTurnedRight_ReportsCorrectPosition()
+        {
+            var robot = new Robot();
+            robot.Place(1, 1, Facing.North);
+            robot.Right();
+            Assert.AreEqual("1,1,EAST", robot.Report());
+        }
+    }
+}
--- TableTopRobo/Robot.cs
+++ TableTopRobo/Robot.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TableTopRobo
 {
-    class Robot
+    public class Robot
     {
 
         public Robot()
